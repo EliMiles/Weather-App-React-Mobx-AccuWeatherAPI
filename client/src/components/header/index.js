f294eddb61351be3f053a284c93504d71e4aa005
Eli Miles <elimiles1987@gmail.com>
2019-10-13 13:44:49 +0300

    improving header

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -6,38 +6,10 @@ import './style.css';
 
 class Header extends Component {
 
-    constructor(props){
-        super(props)
-
-        this.state = {
-            stopWindowSizing: false
-        }
-    }
-
-    //TODO : give it another try later! - prevevet window sizing
-    componentDidMount(){
-        this.displayWindowDimensions();
-        window.addEventListener("resize", this.displayWindowDimensions.bind(this));
-    }
-    displayWindowDimensions() {
-        if(window.innerWidth < 188 && this.state.stopWindowSizing === false){
-            this.setState({stopWindowSizing:true});
-        }
-        else{
-            if(window.innerWidth >= 188 && this.state.stopWindowSizing === true){
-                this.setState({stopWindowSizing:false});
-            }
-        }
-        
-        //console.log(window.innerWidth + ' ' + this.state.stopWindowSizing);
-    }
-    ///////////////////////////////////////////////////////////////////
-
     render() {
         return (
             <Navbar bg="info" expand="true">
-                <span hidden={this.state.stopWindowSizing} className="Navbar-title">Weather App</span>
-                <span hidden={!this.state.stopWindowSizing} className="Navbar-title-small-window">Weather App</span>
+                <span className="Navbar-title">Weather App</span>
                 <span className="Navbar-buttons">
                     <Link to="/" className="Navbar-button"><Button  variant="outline-light" active={window.location.pathname === '/'}>Home</Button></Link>
                     <Link to="/favorites" className="Navbar-button"><Button  variant="outline-light" active={window.location.pathname === '/favorites'}>Favorites</Button></Link>
