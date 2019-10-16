44bca77dd1e718f5f6229e2179252b8bdd4c062a
Eli Miles <elimiles1987@gmail.com>
2019-10-16 15:56:27 +0300

    improvig the homepage display

--- client/src/components/displayResult/index.js
+++ client/src/components/displayResult/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { inject, observer } from 'mobx-react';
 import { WiCelsius } from "react-icons/wi";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import FiveDaysForecast from '../fiveDaysForecast';
 import apiKeys from '../../apiKeys.js'; // (.gitignore) src/apiKeys.js => module.exports = {AccuWeatherKey : 'your key'}
@@ -24,34 +26,68 @@ class DisplayResult extends Component {
     }
 
     render() {
-        this.getTemperature().then(res => {
-            if(res !== undefined){
-                if(res.data.length > 0){
-                    const name = this.props.CurrentSelectedCityStore.getCurrentSelctedCity.name;
-                    const key = this.props.CurrentSelectedCityStore.getCurrentSelctedCity.key;
-                    const temperatureMetricValue = res.data[0].Temperature.Metric.Value;
-                    const temperatureMetricUnit = res.data[0].Temperature.Metric.Unit;
-                    this.props.CurrentSelectedCityStore.changeCurrentSelctedCity({
-                        name:name,
-                        key:key,
-                        temperatureMetricValue:temperatureMetricValue,
-                        temperatureMetricUnit:temperatureMetricUnit
-                    });
-                }
-            }
-            else{
-                console.log('Error - can not find the temperature of this city !');
-            }
-        }, reason => {
-            console.error(reason); // Error!
+        // this.getTemperature().then(res => {
+        //     if(res !== undefined){
+        //         if(res.data.length > 0){
+        //             const name = this.props.CurrentSelectedCityStore.getCurrentSelctedCity.name;
+        //             const key = this.props.CurrentSelectedCityStore.getCurrentSelctedCity.key;
+        //             const temperatureMetricValue = res.data[0].Temperature.Metric.Value;
+        //             const temperatureMetricUnit = res.data[0].Temperature.Metric.Unit;
+        //             this.props.CurrentSelectedCityStore.changeCurrentSelctedCity({
+        //                 name:name,
+        //                 key:key,
+        //                 temperatureMetricValue:temperatureMetricValue,
+        //                 temperatureMetricUnit:temperatureMetricUnit
+        //             });
+        //         }
+        //     }
+        //     else{
+        //         console.log('Error - can not find the temperature of this city !');
+        //     }
+        // }, reason => {
+        //     console.error(reason); // Error!
+        // });
+
+        this.props.CurrentSelectedCityStore.changeCurrentSelctedCity({
+            name:'paris',
+            key:'1234',
+            temperatureMetricValue:'15',
+            temperatureMetricUnit:'C'
         });
         
+        // PrecipitationType
+
         return (
-            <div>
-                <div>{this.props.CurrentSelectedCityStore.getCurrentSelctedCity.name}</div>
-                <div>{this.props.CurrentSelectedCityStore.getCurrentSelctedCity.temperatureMetricValue}<WiCelsius className="WiDegrees" /></div>
-                <FiveDaysForecast />
-            </div>
+            <Container>
+                <Row>
+                    <Col xs="auto" sm="auto" md="auto" large="auto" xl="auto">
+                        <Row>
+                            <Col>
+                                {this.props.CurrentSelectedCityStore.getCurrentSelctedCity.name}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                {this.props.CurrentSelectedCityStore.getCurrentSelctedCity.temperatureMetricValue}<WiCelsius className="WiCelsius" />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col></Col>
+                    <Col xs="auto" sm="auto" md="auto" large="auto" xl="auto">
+                        <FaRegHeart className="hearts" hidden={false} />
+                        <FaHeart className="hearts" hidden={true} />
+                        <span className="between-heart-and-buttons"></span>
+                        <Button hidden={false} variant="outline-info">Add to Favorites</Button>
+                        <Button hidden={true} variant="outline-info">Remove from Favorites</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="PrecipitationType">PrecipitationType</Col>
+                </Row>
+                <Row>
+                    <Col className="FiveDaysForecast"><FiveDaysForecast /></Col>
+                </Row>
+            </Container>
         )
     }
 }
