1a54dd4dd932ebdfc3f60fd7c0dbbc1fe128b4a5
Eli Miles <elimiles1987@gmail.com>
2019-10-17 19:03:49 +0300

    starting of displaying favorites

--- client/src/components/favorites/index.js
+++ client/src/components/favorites/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react';
+import axios from 'axios';
+import { WiCelsius } from "react-icons/wi";
 
+import apiKeys from '../../apiKeys.js'; // (.gitignore) src/apiKeys.js => module.exports = {AccuWeatherKey : 'your key'}
+import FavoriteCity from '../favoriteCity';
+
+@inject('FavoritesStore')
+@inject('CityKeysStore')
+@observer
 class Favorites extends Component {
     render() {
+        //console.log(this.props.FavoritesStore.getFavoriteCities.length);
         return (
             <div>
-                Favorites
+                {this.props.FavoritesStore.getFavoriteCities.map(cityName => {
+                    return <FavoriteCity key={cityName} name={cityName} />
+                })}
             </div>
         )
     }
