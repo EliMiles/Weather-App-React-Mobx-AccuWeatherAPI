044b0d33a5e2a29e115d39331274f7d8289decbf
Eli Miles <elimiles1987@gmail.com>
2019-10-20 15:01:32 +0300

    improvment of favorites page

--- client/src/components/favoriteCity/index.js
+++ client/src/components/favoriteCity/index.js
@@ -1,11 +1,83 @@
 import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react';
+import { Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { WiCelsius } from "react-icons/wi";
 
+import apiKeys from '../../apiKeys.js'; // (.gitignore) src/apiKeys.js => module.exports = {AccuWeatherKey : 'your key'}
+
+import './style.css';
+
+@inject('CurrentSelectedCityStore')
+@observer
 class FavoriteCity extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            temperatureMetricValue:'',
+            WeatherText:''
+        }
+    }
+
+    componentDidMount(){
+        this.getTemperature().then(res => {
+            if(res !== undefined){
+                if(res.data.length > 0){
+                    const obj = {
+                        temperatureMetricValue:res.data[0].Temperature.Metric.Value,
+                        WeatherText:res.data[0].WeatherText
+                    }
+                    this.setState(obj);
+                }
+            }
+            else{
+                console.log('Error - can not find the temperature of this city !');
+            }
+        }, reason => {
+            console.error(reason); // Error!
+        });
+    }
+
+    openCityInHomepage = () => {
+        this.props.CurrentSelectedCityStore.changeCurrentSelctedCity({
+            name:this.props.cityName,
+            key:this.props.cityKey,
+            temperatureMetricValue:'',
+            temperatureMetricUnit:'',
+            WeatherText:'',
+            WeatherIcon:''
+        });
+    }
+
+    getTemperature = async () => {
+        let ans = null;
+        const currentCityKey = this.props.cityKey;
+        if(currentCityKey !== null && currentCityKey !== undefined){
+            ans = await axios.get('http://dataservice.accuweather.com/currentconditions/v1/' + currentCityKey + '?apikey=' + apiKeys.AccuWeatherKey);
+            console.log('Current Conditions endpoint was activated !');
+        }
+        return ans;
+    }
+
     render() {
+
         return (
-            <div>
-                {this.props.cityName} {this.props.cityKey}
-            </div>
+            <Link to="/">
+                <Button className="singleFavoriteCity" onClick={(e) => this.openCityInHomepage()} variant="outline-info" active={this.props.CurrentSelectedCityStore.getCurrentSelctedCity.name === this.props.cityName}>
+                    <div>
+                        {this.props.cityName}
+                    </div>
+                    <div>
+                        {this.state.temperatureMetricValue}<WiCelsius className="WiCelsius" />
+                    </div>
+                    <div>
+                        {this.state.WeatherText}
+                    </div>
+                </Button>
+            </Link>
         )
     }
 }
--- client/src/components/favorites/index.js
+++ client/src/components/favorites/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react';
-import axios from 'axios';
-import { WiCelsius } from "react-icons/wi";
 
-import apiKeys from '../../apiKeys.js'; // (.gitignore) src/apiKeys.js => module.exports = {AccuWeatherKey : 'your key'}
 import FavoriteCity from '../favoriteCity';
 
 @inject('FavoritesStore')
